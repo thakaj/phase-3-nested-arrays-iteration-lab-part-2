d9fa56347fc9c256a4ee0e78fd75971663f19268
thakaj <taulant.hakaj@scranton.edu>
2021-07-31 15:14:25 -0400

    commit tree 73c077b033d0d09fa7b2738fb3d4ee8076c6c4d8

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,19 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  row_index = 0
+  new_array= []
+  while row_index < src.count do
+    element_index = 0
+    min_weather = 100
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index] < min_weather
+        min_weather = src[row_index][element_index]
+    end
+    element_index += 1
+  end
+  new_array << min_weather
+  row_index += 1
+  end
+  new_array
 end
